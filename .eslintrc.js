37e7b11a93a8f58427a0dc0840c2a4e50381fe9f
Laura Ferrando <lauraferrandof@gmail.com>
2021-04-18 18:40:31 +0200

    chore: add Prettier and integration with ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-    node: true,
-  },
-  extends: [
-    'airbnb',
-  ],
-  plugins: [
-    'import',
-    'jsx-a11y',
-    'react',
-    'react-hooks',
-  ],
+	env: {
+		browser: true,
+		node: true
+	},
+	extends: ['airbnb', 'plugin:prettier/recommended'],
+	plugins: ['import', 'jsx-a11y', 'react', 'react-hooks']
 };
