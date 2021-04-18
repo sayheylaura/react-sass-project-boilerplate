52865f4517e37dc6a4161a6d891910ae926309ce
Laura Ferrando <lauraferrandof@gmail.com>
2021-04-18 16:57:26 +0200

    chore: install basic ESLint config with Airbnb styleguide

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  plugins: [
+    'import',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+  ],
+};
