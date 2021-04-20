aa07759914f2423e0b8bb7b915399558dd6503c4
Laura Ferrando <lauraferrandof@gmail.com>
2021-04-20 22:42:17 +0200

    chore: add babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+	parser: '@babel/eslint-parser',
 	env: {
 		browser: true,
 		node: true
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,18 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: true,
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-transform-runtime']
+					}
+				}
+			},
+			{
 				test: /\.css$/i,
 				use: ['style-loader', 'css-loader']
 			},
