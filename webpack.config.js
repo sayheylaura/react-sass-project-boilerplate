e08b3775c63827322bcf097da9c078e78eec68d4
Laura Ferrando <lauraferrandof@gmail.com>
2021-04-20 22:55:39 +0200

    chore: add sass

--- webpack.config.js
+++ webpack.config.js
@@ -29,8 +29,8 @@ module.exports = {
 				}
 			},
 			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader']
+				test: /\.sass$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader']
 			},
 			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
