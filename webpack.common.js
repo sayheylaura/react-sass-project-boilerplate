d41408edfb18020fa7371e0d39ac446507716db4
Laura Ferrando <lauraferrandof@gmail.com>
2021-09-26 22:09:52 +0200

    chore: fix webpack rule for javascript files

--- webpack.common.js
+++ webpack.common.js
@@ -14,13 +14,11 @@ module.exports = {
 			{
 				test: /\.js$/,
 				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						cacheDirectory: true,
-						presets: ['@babel/preset-env', '@babel/preset-react'],
-						plugins: ['@babel/plugin-transform-runtime']
-					}
+				loader: 'babel-loader',
+				options: {
+					cacheDirectory: true,
+					presets: ['@babel/preset-env', '@babel/preset-react'],
+					plugins: ['@babel/plugin-transform-runtime']
 				}
 			},
 			{
