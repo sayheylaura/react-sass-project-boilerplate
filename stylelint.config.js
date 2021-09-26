4391e00681825b8eeced5fd275a3df8e75c69b06
Laura Ferrando <lauraferrandof@gmail.com>
2021-09-26 19:52:40 +0200

    chore: update stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,8 +2,25 @@ module.exports = {
 	defaultSeverity: 'warning',
 	extends: 'stylelint-config-standard',
 	plugins: ['stylelint-order', 'stylelint-scss'],
+	ignoreFiles: ['./node_modules'],
 	rules: {
+		'block-closing-brace-empty-line-before': null,
+		'block-closing-brace-newline-after': null,
+		'block-closing-brace-newline-before': null,
+		'block-closing-brace-space-after': null,
+		'block-closing-brace-space-before': null,
+		'block-opening-brace-newline-after': null,
+		'block-opening-brace-newline-before': null,
+		'block-opening-brace-space-after': null,
+		'block-opening-brace-space-before': null,
+		'declaration-block-trailing-semicolon': 'never',
+		'number-leading-zero': 'never',
 		'order/properties-alphabetical-order': true,
-		'declaration-block-trailing-semicolon': 'never'
+		'rule-empty-line-before': [
+			'always',
+			{
+				ignore: ['after-comment']
+			}
+		]
 	}
 };
